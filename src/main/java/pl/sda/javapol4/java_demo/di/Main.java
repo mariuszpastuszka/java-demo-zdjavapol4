e5ca903f4b6cce4324ea24530b997502d40968a1
maniek <mpastuszka@gmail.com>
2020-11-21 12:44:15 +0100

    [add] - dependency injection example

---
+++ src/main/java/pl/sda/javapol4/java_demo/di/Main.java
@@ -0,0 +1,13 @@
+package pl.sda.javapol4.java_demo.di;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Engine small = new Engine(800);
+        String color = "pink";
+
+        Car myFavouriteCar = new Car(small, color);
+
+        Car defaultCar = new Car();
+    }
+}
